be5c437988791907c4044019d4a70d38b5c3619d
peculiar19 <gemma192001@gmail.com>
2023-06-25 13:10:37 +0800

    update

--- src/Screens/Category.js
+++ src/Screens/Category.js
@@ -15,6 +15,17 @@ const Category = () => {
       });
   }, []);
 
+const handleDelete = (categoryId) => {
+    axios.delete(`http://localhost:8000/api/v1/inventory/api/categories/${categoryId}/`)
+      .then(response => {
+        // Filter out the deleted category from the state
+        setCategories(categories.filter(category => category.id !== categoryId));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div align="center">
       <h1 class="title">Category List</h1>
@@ -23,6 +34,7 @@ const Category = () => {
           <tr>
             <th>Category #</th>
             <th>Type</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody class="table-body">
@@ -30,6 +42,9 @@ const Category = () => {
             <tr key={category.id}>
               <td>{category.id}</td>
               <td>{category.type}</td>
+              <td>
+                <button onClick={() => handleDelete(category.id)}>Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
